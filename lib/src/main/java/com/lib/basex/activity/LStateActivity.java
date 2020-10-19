b524e147199f1c98d9dc1bc60a285c6a31f93a8b
Alan <tonyyxm@qq.com>
2020-10-19 16:03:05 +0800

    init

--- lib/src/main/java/com/lib/basex/activity/LStateActivity.java
+++ lib/src/main/java/com/lib/basex/activity/LStateActivity.java
@@ -1,6 +1,7 @@
 package com.lib.basex.activity;
 
 import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.databinding.ViewDataBinding;
@@ -20,8 +21,8 @@ public abstract class LStateActivity<T extends LViewModel, D extends ViewDataBin
     @Override
     protected void initContentView() {
         binding = DataBindingUtil.setContentView(this, R.layout.l_activity_state);
-        d = DataBindingUtil.inflate(LayoutInflater.from(this),getContentId(),binding.stateLayout,true);
-
+        d = DataBindingUtil.inflate(LayoutInflater.from(this), getContentId(), null, false);
+        binding.stateLayout.addView(d.getRoot(), new RelativeLayout.LayoutParams(-1, -1));
     }
 
     public void setTitleBar(String title) {
